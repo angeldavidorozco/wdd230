5e9cb953fdada77a960699dae987515ff35cb31c
Angel Orozco <angeldorozcon@gmail.com>
2023-09-13 18:42:04 -0300

    Creates initial build

---
+++ scripts/getdates.js
@@ -0,0 +1,15 @@
+
+let lastModified =  document.lastModified;
+
+document.getElementById("lastModified").innerText = `Last modification: ${lastModified}`
+
+
+let currentDate = new Date();
+
+let year = currentDate.getFullYear();
+
+let footer = document.querySelector("footer");
+
+let firstP = footer.firstElementChild;
+
+firstP.innerHTML = `&copy; ${year}<br>Angel Orozco<br>Argentina <img src="styles/images/argentina-flag-icon.svg" alt="flag">`
