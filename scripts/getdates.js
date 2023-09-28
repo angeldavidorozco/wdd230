37db0f14b1bcf2092fb4f76650eac52404f51b9a
Angel Orozco <angeldorozcon@gmail.com>
2023-09-28 09:37:42 -0300

    Finished group activity

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -13,3 +13,16 @@ let footer = document.querySelector("footer");
 let firstP = footer.firstElementChild;
 
 firstP.innerHTML = `&copy; ${year}<br>Angel Orozco<br>Argentina <img src="styles/images/argentina-flag-icon.svg" alt="flag">`
+
+
+//hamburguer button
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const map = document.querySelector('#map');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+    map.classList.toggle('open');
+});
