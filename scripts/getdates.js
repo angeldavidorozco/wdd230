8ff829a82b30592b0d8ade80cd0e730f936cb2ee
Angel Orozco <angeldorozcon@gmail.com>
2023-10-12 18:30:30 -0300

    Dark mode added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -26,3 +26,47 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
     map.classList.toggle('open');
 });
+
+
+
+// Dark mode
+
+const modeButton = document.querySelector(".slider");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const titles = document.querySelectorAll(".card h3");
+const cards = document.querySelectorAll(".card");
+const foot = document.querySelector("footer");
+const nav = document.querySelector("nav");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.style.backgroundColor === "") {
+		body.style.background = "#000";
+		body.style.color = "#fff";
+		modeButton.style.backgroundColor = "#2196F3";
+		header.style.backgroundColor = "#0e273b";
+		titles.forEach(element => {
+			element.style.backgroundColor = '#0e273b';
+		});
+		cards.forEach(element => {
+			element.style.backgroundColor = '#7b7c7d';
+		});
+		foot.style.backgroundColor = "#0e273b";
+		nav.style.backgroundColor = "#7b7c7d";
+		
+	} else if (modeButton.style.backgroundColor != 'white'){
+		body.style.background = "#fff";
+		body.style.color = "#000";
+		modeButton.style.backgroundColor = "";
+		header.style.backgroundColor = "#1d4d72";
+		titles.forEach(element => {
+			element.style.backgroundColor = '#1d4d72';
+		});
+		cards.forEach(element => {
+			element.style.backgroundColor = 'white';
+		});
+		foot.style.backgroundColor = "#1d4d72";
+		nav.style.backgroundColor = "black";
+		
+	}
+});
